737b15a0f287facff76028490a5608d9cef783d1
hashimzs <hashimmohd28@gmail.com>
2024-04-13 18:08:36 +0300

    update

--- chatbot2/Program.cs
+++ chatbot2/Program.cs
@@ -1,18 +1,26 @@
 using Blazored.LocalStorage;
+using Blazored.Toast;
 using chatbot2;
 using chatbot2.Services;
 using Ljbc1994.Blazor.IntersectionObserver;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7127/") });
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7127/") }
+    .EnableIntercept(sp));
+
 builder.Services.AddScoped<ChatService>();
 builder.Services.AddScoped<AuthService>();
 builder.Services.AddIntersectionObserver();
+builder.Services.AddBlazoredToast();
 builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddHttpClientInterceptor(); // <- Add this!
+
 
 await builder.Build().RunAsync();
